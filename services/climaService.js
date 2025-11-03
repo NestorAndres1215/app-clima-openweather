0a278b64c33b9bb6b5cd51f5303fca17907a26e2
NestorAndres1215 <atiroandres@gmail.com>
2025-11-02 22:09:09 -0500

    subiendo

--- services/climaService.js
+++ services/climaService.js
@@ -1,16 +1,42 @@
 const axios = require('axios');
 
+const MENSAJES = {
+    ERROR_API_KEY: 'API key de OpenWeather no configurada.',
+    ERROR_DATOS_INCOMPLETOS: 'Datos de clima incompletos.',
+    ERROR_CIUDAD_NO_ENCONTRADA: 'Ciudad no encontrada.',
+    ERROR_CONEXION: 'No se pudo conectar al servicio de clima.',
+    ERROR_GENERAL: 'Error al obtener los datos de clima.'
+};
+
 exports.obtenerClima = async (ciudad) => {
     const apiKey = process.env.OPENWEATHER_API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=${apiKey}&lang=es`;
+    if (!apiKey) throw new Error(MENSAJES.ERROR_API_KEY);
 
-    const respuesta = await axios.get(url);
-    const data = respuesta.data;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(ciudad)}&units=metric&appid=${apiKey}&lang=es`;
 
-    return {
-        ciudad: data.name,
-        temperatura: data.main.temp,
-        descripcion: data.weather[0].description,
-        icono: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    };
+    try {
+        const respuesta = await axios.get(url);
+        const data = respuesta.data;
+
+        if (!data || !data.main || !data.weather || !data.weather[0]) {
+            throw new Error(MENSAJES.ERROR_DATOS_INCOMPLETOS);
+        }
+
+        return {
+            ciudad: data.name,
+            temperatura: data.main.temp,
+            descripcion: data.weather[0].description,
+            icono: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+            humedad: data.main.humidity,
+            viento: data.wind?.speed || 0
+        };
+    } catch (err) {
+        if (err.response && err.response.status === 404) {
+            throw new Error(MENSAJES.ERROR_CIUDAD_NO_ENCONTRADA);
+        } else if (err.code === 'ENOTFOUND' || err.code === 'ECONNABORTED') {
+            throw new Error(MENSAJES.ERROR_CONEXION);
+        } else {
+            throw new Error(err.message || MENSAJES.ERROR_GENERAL);
+        }
+    }
 };
